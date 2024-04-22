e548516e143f39eb83922fb377f4771d88c2aaa6
Mahomed Cissokho <mahomedcissokho@esp.sn>
2024-04-22 20:25:17 +0000

    big changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Accueil from "./Pages/Accueil"
+import Connexion from "./Pages/Connexion"
+import Home from "./Pages/Home"
+import Candidat from "./Pages/Candidat"
+import Monitoring from "./Pages/Monitoring"
+import Authentification from "./Pages/Authentification"
+import NouveauCandidat from "./Pages/NouveauCandidat"
+
 export default function App() {
   return (
-    <h1 className="text-xl font-bold ">
-      Hello world!
-    </h1>
-  )
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Accueil />} />
+          <Route path="/connexion" element={<Connexion />} />
+          <Route path="/accueil" element={<Home />} />
+          <Route path="/candidat" element={<Candidat />} />
+          <Route path="/candidat/nouveau" element={<NouveauCandidat />} />
+          
+          <Route path="/monitoring" element={<Monitoring />} />
+          <Route path="/auth" element={<Authentification />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
